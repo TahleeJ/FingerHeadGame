f9b6029404d8d1f650c1ac48b549c78aea60ec2f
TahleeJ <tahlee.jaynes@gmail.com>
2019-11-16 22:57:31 -0500

    leaderboard functionality

--- Trial App/flask_blog.py
+++ Trial App/flask_blog.py
@@ -1,3 +1,28 @@
+from flask import request, Flask, render_template, flash, session, redirect
+# from flask.ext.session import Session
+
+class Score:
+    def __init__(self, score, gamerTag):
+        self.score = score
+        self.gamerTag = gamerTag
+
+class Leaderboard:
+    def __init__(self):
+        self.board=[Score(0, "TBD")]
+
+leaderboard = Leaderboard()
+leaderboard.board.sort(key=lambda x:x.score, reverse=True)
+
+class User:
+    def __init__(self, firstName, lastName, gamerTag):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.gamerTag = gamerTag
+        temp = Score(0, gamerTag)
+        leaderboard.board.append(temp)
+        # leaderboard.board.append(Score(0, gamerTag))
+        # leaderboard.board.append({0: gamerTag})
+
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -16,10 +41,12 @@ posts = [
     }
 ]
 
+# scores = [leaderboard.keys()]
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', posts=posts)
+    return render_template('home.html', posts=posts, leaderboard=sorted(leaderboard.board, key=lambda x: int(x.score), reverse=True))
 
 @app.route("/about")
 def about():
@@ -28,6 +55,31 @@ def about():
 @app.route('/webcam')
 def webcam():
     return render_template('webcam.html', title='Webcam')
+
+@app.route('/newUser', methods=['GET', 'POST'])
+def newUser():
+    if request.method == 'POST':
+        firstName = request.form['firstName']
+        lastName = request.form['lastName']
+        gamerTag = request.form['gamerTag']
+        currentUser = User(firstName, lastName, gamerTag)
+        # flash("Welcome")
+        return redirect("/home")
+    else:
+        return render_template('new_user.html', title='NewUser')
+
+@app.route('/testUser', methods =['GET', 'POST'])
+def testUser():
+    if request.method == 'POST':
+        firstName = request.form['firstName']
+        lastName = request.form['lastName']
+        gamerTag = request.form['gamerTag']
+        score = request.form['score']
+        leaderboard.board.append(Score(score, gamerTag))
+            # flash("Welcome")
+        return redirect("/home")
+    else:
+        return render_template('test_user.html', title='testUser')
 
 if __name__ == '__main__':
     app.run(debug=True)
